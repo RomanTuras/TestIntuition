2e3764266842f89855ca20541b69a947f72a87e9
salden <salden.com@gmail.com>
2015-11-25 14:13:44 +0200

    Add file: arrays.xml, methods: onClickItem (ListView), change initMainListItem

--- app/src/main/java/ua/com/spasetv/testintuitions/ListData.java
+++ app/src/main/java/ua/com/spasetv/testintuitions/ListData.java
@@ -2,6 +2,18 @@ package ua.com.spasetv.testintuitions;
 
 /**
  * Created by salden on 25/11/2015.
+ * Container for List View in main activity
  */
 public class ListData {
+    String title;
+    String amountTimes;
+    String bestResult;
+    int idImg;
+
+    public ListData(String title, String amountTimes, String bestResult, int idImg){
+        this.title = title;
+        this.amountTimes = amountTimes;
+        this.bestResult = bestResult;
+        this.idImg = idImg;
+    }
 }
--- app/src/main/java/ua/com/spasetv/testintuitions/MainActivity.java
+++ app/src/main/java/ua/com/spasetv/testintuitions/MainActivity.java
@@ -1,15 +1,23 @@
 package ua.com.spasetv.testintuitions;
 
+import android.content.res.Resources;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ListView;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity implements AdapterView.OnItemClickListener {
+
+    ArrayList<ListData> arrayList;
+    MyAdapter myAdapter;
+    ListView mainListItem;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,19 +26,42 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+        mainListItem = (ListView) findViewById(R.id.listView);
+
+        initMainListItems();
+        myAdapter = new MyAdapter(this, arrayList);
+        mainListItem.setAdapter(myAdapter);
+        mainListItem.setOnItemClickListener(this);
+    }
+
+    public void initMainListItems(){
+        arrayList = new ArrayList<>();
+        Resources res = getResources();
+        int amountTimes = 0;
+        int bestResult = 0;
+        String dateOfBestResult = "01.01.1953";
+        String [] titleExercises = getResources().getStringArray(R.array.titleExercise);
+
+        arrayList.add(new ListData(res.getString(R.string.titleAbout),
+                res.getString(R.string.descriptionAbout), "", R.drawable.ic_blank));
+
+        for (String titleEx: titleExercises) {
+            arrayList.add(new ListData(titleEx,
+                    res.getString(R.string.amount)+" "+amountTimes+" "+
+                            res.getString(R.string.times),
+                    res.getString(R.string.bestResult)+" "+bestResult+
+                            res.getString(R.string.was)+" "+dateOfBestResult,
+                    R.drawable.ic_blank));
+        }
+
+        arrayList.add(new ListData(res.getString(R.string.titleEnableStat),
+                res.getString(R.string.descriptionStat),
+                res.getString(R.string.descriptionStat2),
+                R.drawable.ic_blank));
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_main, menu);
         return true;
     }
@@ -48,5 +79,10 @@ public class MainActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+        Log.d("TG", "pos - "+i);
     }
 }
